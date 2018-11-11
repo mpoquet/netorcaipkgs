dc5b57fccc4bbd352585c32644b00451bf14e83c
Millian Poquet <millian.poquet@inria.fr>
2018-11-11 23:25:04 +0100

    [nix] allow netorcaipkgs options

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
-let
-  pkgs = import
-    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.09.tar.gz") {};
-  kapack = import
+{
+  pkgs ? import
+    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.09.tar.gz") {},
+  kapack ? import
     ( fetchTarball "https://github.com/oar-team/kapack/archive/fe342846a8dfccc85077798357f9d7a9e889a8c7.tar.gz")
-  { inherit pkgs; };
-in
-
+  { inherit pkgs; }
+}:
 let
   # Add libraries to the scope of callPackage
   callPackage = pkgs.lib.callPackageWith (kapack // pkgs // pkgs.xlibs // self);
